535392ca421d9b5c88f0b93332a47f55f056f9b6
Soucelover <mr.ktotov@gmail.com>
2026-04-15 19:03:30 +0300

    Move out CLI related code

--- src/mindustry_pal/__main__.py
+++ src/mindustry_pal/__main__.py
@@ -1,38 +1,8 @@
-from argparse import ArgumentParser
-
-from .campaigns import create, restore, state, store, switch
-
-name = "manage.py"
-usage = None
-description = "Manager made for mindustry game."
-epilog = "epi"
+from .cli import cli
 
 
-def main(args: list[str] | None = None) -> None:
-    parser = ArgumentParser(name, usage, description, epilog)
-    parsers = parser.add_subparsers(metavar="command", required=True)
-
-    store_parser = parsers.add_parser("store", help=store.__doc__)
-    store_parser.add_argument("name", nargs="?", help="Name of campaign")
-    store_parser.set_defaults(command=store)
-
-    restore_parser = parsers.add_parser("restore", help=store.__doc__)
-    restore_parser.add_argument("name", nargs="?", help="Name of campaign")
-    restore_parser.set_defaults(command=restore)
-
-    create_parser = parsers.add_parser("create", help=create.__doc__)
-    create_parser.add_argument("name", help="Name of new campaign")
-    create_parser.set_defaults(command=create)
-
-    switch_parser = parsers.add_parser("switch", help=switch.__doc__)
-    switch_parser.add_argument("name")
-    switch_parser.set_defaults(command=switch)
-
-    state_parser = parsers.add_parser("state", help=state.__doc__)
-    state_parser.set_defaults(command=state)
-
-    parsed = parser.parse_args(args)
-    parsed.command(parsed)
+def main() -> None:
+    cli()
 
 
 if __name__ == "__main__":
