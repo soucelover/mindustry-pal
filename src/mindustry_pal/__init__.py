c3ed0946e26d193d53f1cca7ce9d3b9a3caea305
Soucelover <mr.ktotov@gmail.com>
2026-06-11 13:59:13 +0300

    Add __version__ to mindustry_pal.__init__

--- src/mindustry_pal/__init__.py
+++ src/mindustry_pal/__init__.py
@@ -1,1 +1,11 @@
 """A CLI for managing Mindustry game versions and campaigns."""
+
+from importlib import metadata
+
+__all__ = ["__version__"]
+PACKAGE_NAME = "mindustry-pal"
+
+try:
+    __version__ = metadata.version(PACKAGE_NAME)
+except metadata.PackageNotFoundError:
+    __version__ = "0.1.dev1+UNKNOWN"
